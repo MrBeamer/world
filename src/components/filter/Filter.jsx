96326c38f4c3249cdcffae22e3db0b19fc7180a4
michael beamer <beamermichael@gmail.com>
2021-12-25 19:55:21 +0100

    added card hover effects

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -3,20 +3,23 @@ import "./filter.css";
 
 export default function Filter({ theme, onFilterChange }) {
   return (
-    <select
-      onChange={onFilterChange}
-      id="continent"
-      name="continent"
-      className={`filter__select ${theme}`}
-      defaultValue="Filter by Region"
-      aria-label="filter continent"
-    >
-      <option value="All">Filter by Region</option>
-      <option value="Africa">Africa</option>
-      <option value="Americas">America</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
-    </select>
+    <div className="filter__wrapper">
+      <select
+        onChange={onFilterChange}
+        id="continent"
+        name="continent"
+        className={`filter__select ${theme}`}
+        defaultValue="Filter by Region"
+        aria-label="filter continent"
+      >
+        <option value="All">Filter by Region</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">America</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+      <i class="fas fa-angle-down filter__icon"></i>
+    </div>
   );
 }
